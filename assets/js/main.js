42046f5f774f3bb6f7d8349e841e1549edddae7e
William Fischdick <jwfbit@hotmail.com>
2023-08-22 22:54:57 -0300

    :construction: feat: Implementando a lógica para criar os cards utilizando a API.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,36 @@ let currentPageUrl = 'https://swapi.dev/api/people/'
 async function loadCharacters (url) {
     
     const mainContent = document.getElementById('main-content');
-
     mainContent.innerHTML = '';
+
+    try {
+        const response = await fetch(url);
+        const responseJson =  await response.jason();
+
+        responseJson.results.forEach((character) => {
+            const card = document.createElement("div");
+            card.style.backgroundImage = `url('https://starwars-visualguide.com/assets/img/characters/1.jpg')`;
+            card.className = "cards";
+            
+            const characterNameBg = document.createElement("div");
+            characterNameBg.className = "character-name-bg";
+
+            const characterName = document.createElement("span");
+            characterName.className = "character-name";
+            characterName.innerText = `${character.name}`;
+
+            characterNameBg.appendChild(characterName);
+            card.appendChild(characterNameBg);
+
+            mainContent.appendChild(card);
+        })
+
+        currentPageUrl = url;
+
+    } catch (error) {
+        console.log(error);
+        alert('Erro ao carregar os cards');
+    }
 }
 
 
@@ -21,6 +49,6 @@ window.onload = async () => {
         await loadCharacters(currentPageUrl);
     } catch (error) {
         console.log(error);
-        alert('Erro ao carregar os cards dos personagens');
+        alert('Erro ao carregar os personagens');
     }
 };
